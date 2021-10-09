f304705f6fa805ceb247af1f3457ee480212f0fc
Kevin-Brandon Joseph Corbett <k-b0514@hotmail.com>
2021-10-09 15:58:31 -0400

    04 - Constructing The Capsule

--- Source/TankGame/BasePawn.cpp
+++ Source/TankGame/BasePawn.cpp
@@ -1,6 +1,7 @@
 // K-B 2021
 
 #include "BasePawn.h"
+#include "Components/CapsuleComponent.h"
 
 // Sets default values
 ABasePawn::ABasePawn()
@@ -8,7 +9,9 @@ ABasePawn::ABasePawn()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	
+	// Construct the body
+	CapsuleComp = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule Collider"));
+	RootComponent = CapsuleComp;
 
 }
 
